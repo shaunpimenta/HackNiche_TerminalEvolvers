9e45a158ca0e939173a41dd6078a5aab585c35bb
Shaun Pimenta <83634457+shaunpimenta@users.noreply.github.com>
2023-02-17 19:29:30 +0530

    Add files via upload

---
+++ views/dashboard/Default/index.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+
+// material-ui
+import {
+    Avatar,
+    AvatarGroup,
+    Box,
+    Button,
+    Grid,
+    List,
+    ListItemAvatar,
+    ListItemButton,
+    ListItemSecondaryAction,
+    ListItemText,
+    MenuItem,
+    Stack,
+    TextField,
+    Typography
+} from '@mui/material';
+
+// project imports
+import EarningCard from './EarningCard';
+import PopularCard from './PopularCard';
+import TotalOrderLineChartCard from './TotalOrderLineChartCard';
+import TotalIncomeDarkCard from './TotalIncomeDarkCard';
+import TotalIncomeLightCard from './TotalIncomeLightCard';
+import TotalGrowthBarChart from './TotalGrowthBarChart';
+import { gridSpacing } from 'store/constant';
+import IncomeAreaChart from './IncomeAreaChart';
+import MainCard from 'ui-component/cards/MainCard';
+
+// ==============================|| DEFAULT DASHBOARD ||============================== //
+
+const Dashboard = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [value, setValue] = useState('today');
+    const [slot, setSlot] = useState('week');
+    useEffect(() => {
+        setLoading(false);
+    }, []);
+
+    return (
+        <Grid container spacing={gridSpacing}>
+            <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item lg={4} md={6} sm={6} xs={12}>
+                        <EarningCard isLoading={isLoading} />
+                    </Grid>
+                    <Grid item lg={4} md={6} sm={6} xs={12}>
+                        <TotalOrderLineChartCard isLoading={isLoading} />
+                    </Grid>
+                    <Grid item lg={4} md={12} sm={12} xs={12}>
+                        <Grid container spacing={gridSpacing}>
+                            <Grid item sm={6} xs={12} md={6} lg={12}>
+                                <TotalIncomeDarkCard isLoading={isLoading} />
+                            </Grid>
+                            <Grid item sm={6} xs={12} md={6} lg={12}>
+                                <TotalIncomeLightCard isLoading={isLoading} />
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+
+            <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} md={8}>
+                        <Grid item>
+                            <Typography variant="h5">Income and Expenses</Typography>
+                        </Grid>
+                        <Stack direction="row" alignItems="center" spacing={0}>
+                            <Button
+                                size="small"
+                                onClick={() => setSlot('month')}
+                                color={slot === 'month' ? 'primary' : 'secondary'}
+                                variant={slot === 'month' ? 'outlined' : 'text'}
+                            >
+                                Month
+                            </Button>
+                            <Button
+                                size="small"
+                                onClick={() => setSlot('week')}
+                                color={slot === 'week' ? 'primary' : 'secondary'}
+                                variant={slot === 'week' ? 'outlined' : 'text'}
+                            >
+                                Week
+                            </Button>
+                        </Stack>
+                        <MainCard content={false} sx={{ mt: 1 }}>
+                            <Box sx={{ pt: 1, pr: 1 }}>
+                                <IncomeAreaChart slot={slot} />
+                            </Box>
+                        </MainCard>
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <PopularCard isLoading={isLoading} />
+                    </Grid>
+                </Grid>
+            </Grid>
+            {/* <Grid item xs={12}>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12} md={8}>
+                        <TotalGrowthBarChart isLoading={isLoading} />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                        <PopularCard isLoading={isLoading} />
+                    </Grid>
+                </Grid>
+            </Grid> */}
+        </Grid>
+    );
+};
+
+export default Dashboard;
